a2ab5ed8828ed4b76ad389150c675103a7c14c7a
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-02-15 19:34:25 +0100

    Update bloques.c- bwrite

--- PracticaSO2/bloques.c
+++ PracticaSO2/bloques.c
@@ -9,18 +9,21 @@ static int descriptor = 0;
 
 
 /**
- * Escribe 1 bloque en el dispositivo virtual, en el bloque físico (nbloque)
- * @par nbloque     posición virtual del bloque
- * @par buf         buffer de memoria cuyo contenido escribiremos en el fichero
- * @return  numero de bytes escrtitos
+ * Escribe 1 bloque en el dispositivo virtual, en el bloque físico (nbloque).
+ * @param nbloque     posición virtual del bloque
+ * @param buf         buffer de memoria cuyo contenido escribiremos en el fichero
+ * @return  numero de bytes escritos 
 */
 int bwrite(unsigned int nbloque, const void *buf){
-int pos = nbloque*BLOCKSIZE;//poision de bloque
+int pos = nbloque*BLOCKSIZE;//poisicon del bloque
 
-lssek(descriptor, pos, SEEK_SET);   //posicionar el puntero dentro del bloque
-int nbytes = write(descriptor,buf,BLOCKSIZE);//escribir en el bloque
+if (lssek(descriptor, pos, SEEK_SET)<0){   //posicionar el puntero dentro del bloque
+    fprintf(stderr, RED NEGRITA"Error al posicionar el puntero.\n"RESET);
+}
+//Escribir en el bloque
+int nbytes = write(descriptor,buf,BLOCKSIZE);
 if (nbytes<0){
-    fprintf(stderr,RED"Error al escribir" RESET);
+    fprintf(stderr,RED"Error al escribir el bloque %i.\n" RESET, nbloque);
         return FALLO;
 }else{
     //numero de bytes escritos en el fichero
