2066f8cb30e0e37a690cfef65c9a98fbeb9b6881
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-02-15 11:04:03 +0100

    Add files via upload

---
+++ PracticaSO2/mi_mkfs.c
@@ -0,0 +1,18 @@
+//Formatea el dispositivo virtual con el tamaño adecuado (nbloques).
+
+
+/**
+ * Programa principal
+ *Recibe tres parametros. 
+ * @param   argc numero de parametros en argv
+ * @param   argv    cadena de comandos
+ * @return 0 en caso de existo
+*/
+ 
+
+int main(int argc, char **argv){
+
+
+
+
+}
