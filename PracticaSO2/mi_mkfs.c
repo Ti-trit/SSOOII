acc191a6f1b9ef89495447633e4d8935850a9977
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-02-16 21:25:20 +0100

    Update mi_mkfs.c

--- PracticaSO2/mi_mkfs.c
+++ PracticaSO2/mi_mkfs.c
@@ -1,5 +1,6 @@
 #include "bloques.h"
 #include <stdio.h>
+#include <string.h>
 
 
 /**
@@ -7,24 +8,45 @@
  * Formatea el dispositivo virtual con el tamaño adecuado de bloques, nbloques. 
  * @param   argc numero de parametros en argv
  * @param   argv    cadena de comandos
- * @return 0 si el programa se ejecuto correctamente
+ * @return  0 si el programa se ejecuto correctamente
 */
  
 
 int main(int argc, char **argv){
 
-char *nombre;
 if (argc!=3){
     fprintf(stderr, RED"Sintaxis incorrecta:./mi_mkfs <nombre_dispositivo> <nbloques>."RESET);
     return FALLO;
 }
 //obtener nombre de dispositivo
-char * nombre = argv[1];
+char * camino = argv[1];
 //numero de bloques
 int nbloques = atoi(argv[2]);
 
 //montar el dispositivo virtual
+  if (bmount(camino)<0){
+    fprintf(stderr, RED "Se ha producido un error al montar el dispositivo");
+    return FALLO;
+  }
 
+    unsigned char buf [nbloques];
+    memset(buf, 0, nbloques);
+  for (int i = 0; i<nbloques; i++){
+   if ( bwrite(i, buf)<1){
+    fprintf(stderr, RED"Error al escribir en el bloque %i"RESET, i);
+    return FALLO;
+   }
+
+  }
+
+  //desmontar el dispositivo virtual
+  if (bmount(camino)<0){
+
+    fprintf(stderr, RED"Error al desmontar el dispositivo" RESET);
+    return FALLO;
+  }
+
+    
 
 
 }
