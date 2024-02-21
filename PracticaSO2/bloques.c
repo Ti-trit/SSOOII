0a912b285f232492a4f6c53fb13edf29bfb0e0a9
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-02-21 20:14:21 +0100

    comentarios con 70 caracteres o menos

--- PracticaSO2/bloques.c
+++ PracticaSO2/bloques.c
@@ -10,13 +10,15 @@ static int descriptor = 0;
 /**
  * Monta el dispositivo virtual, abre el fichero
  * @param camino      nombre del fichero
- * @return          devuelve -1 si ha habido error, o el descriptor si ha ido bien
+ * @return           el descriptor o -1 en caso de error
+ * 
 */
 int bmount(const char *camino){
-    //Configuramos la umask del proceso para no restringir ningun permiso
+    // Configurar umask del proceso para no restringir permisos
     umask(000);
     
-    //Intenta abrir el archivo con permisos de lectura y escritura, creandolo si no existe
+    // abrir el archivo con permisos de lectura y escritura,
+    // creandolo si no existe
     descriptor = open(camino, O_RDWR | O_CREAT, 0666);
 
     //Verifica si hubo un error al abrir el archivo
@@ -24,13 +26,13 @@ int bmount(const char *camino){
         fprintf(stderr, RED "Error al abrir el fichero.\n");
         return FALLO;
     }
-    //Si todo va bien, retorn el descriptor del archivo
+    //Si todo va bien, retorn el descriptor del archivos
     return descriptor;
 }
 
 /**
  * Desmonta el dispositivo virtual
- * @return          devuelve 0 si lo ha cerrado correctamente o -1 en caso contrario
+ * @return           0 si lo ha cerrado correctamente. -1 en otro caso
 */
 int bumount() {
     //Si no se ha cerrado devuelve -1
@@ -44,8 +46,8 @@ int bumount() {
 /**
  * Escribe 1 bloque en el dispositivo virtual, en el bloque físico (nbloque).
  * @param nbloque     posición virtual del bloque
- * @param buf         buffer de memoria cuyo contenido escribiremos en el fichero
- * @return  numero de bytes escritos 
+ * @param buf         buffer de memoria para escribir 
+ * @return            número de bytes escritos 
 */
 int bwrite(unsigned int nbloque, const void *buf){
 int pos = nbloque*BLOCKSIZE;//poisicon del bloque
@@ -59,7 +61,7 @@ if (nbytes<0){
     fprintf(stderr,RED"Error al escribir el bloque %i.\n" RESET, nbloque);
         return FALLO;
 }else{
-    //numero de bytes escritos en el fichero
+    //número de bytes escritos en el fichero
     return nbytes;
 }
 
@@ -68,23 +70,23 @@ if (nbytes<0){
 /**
  * Lee 1 bloque del dispositivo virtual, correspondiente al físico nbloque.
  * @param nbloque     posición virtual del bloque
- * @param buf         buffer de memoria cuyo contenido intentaremos leer.
- * @return  nbytes, número de bytes que leemos, o FALLO(-1) si salta algún error. 
+ * @param buf         buffer de memoria cuyo contenido intentaremos leer
+ * @return              número de bytes leedos.FALLO en caso de error 
 */
 int bread(unsigned int nbloque, void*buf){
-    int pos = nbloque*BLOCKSIZE;// Posición inicial.
+    int pos = nbloque*BLOCKSIZE;// Posición inicial
 
     if (lseek(descriptor, pos, SEEK_SET)<0){
         fprintf(stderr, RED NEGRITA"Error al posicionar el puntero.\n"RESET);
-        return FALLO; // Error al posicionar el puntero.
+        return FALLO; // Error al posicionar el puntero
     }
 
-    // Lee el contenido del bloque.
+    // Lee el contenido del bloque
     int nbytes = read(descriptor,buf,BLOCKSIZE);
     if(nbytes>=0){
-        return nbytes; // Devuelve nbytes == BLOCKSIZE.
+        return nbytes; // Devuelve nbytes == BLOCKSIZE
     }else{
         fprintf(stderr, RED NEGRITA"Error al leer en el bloque %i.\n"RESET, nbloque);
-        return FALLO; // Salta un error en el bloque "nbloque".
+        return FALLO; // Salta un error en el bloque "nbloque"
     }
 }
