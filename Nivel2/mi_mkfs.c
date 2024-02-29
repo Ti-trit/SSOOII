0a63da8d9e7999924e94cda1e2db7315003f23f3
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-02-29 21:26:38 +0100

    He quitado las funciones de ficheros_basico que habia declarado aqui ya que ahora estan en la cabecera de ficheros_basico.h!

--- Nivel2/mi_mkfs.c
+++ Nivel2/mi_mkfs.c
@@ -1,11 +1,5 @@
 #include "ficheros_basico.h"
 
-int tamMB(unsigned int nbloques);
-int tamAI(unsigned int ninodos);
-int initSB(unsigned int nbloques, unsigned int ninodos);
-int initMB(); 
-int initAI();
-
 
 
 /**
