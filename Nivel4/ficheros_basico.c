5c6c770f343bb398860dcd322adf652e3969fa8b
Tgallardo97 <136459112+Tgallardo97@users.noreply.github.com>
2024-03-14 09:23:00 +0100

    Update ficheros_basico.c

--- Nivel4/ficheros_basico.c
+++ Nivel4/ficheros_basico.c
@@ -559,9 +559,9 @@ int obtener_indice(unsigned int nblogico, int nivel_punteros){
 
     if(nblogico<DIRECTOS){ //Dentro de los bloques directos.
         return nblogico;
-    }else if(DIRECTOS<=nblogico<INDIRECTOS0){ //Dentro de los bloques indirectos 0.
+    }else if((DIRECTOS<=nblogico)&&(nblogico<INDIRECTOS0)){ //Dentro de los bloques indirectos 0.
         return nblogico-DIRECTOS;
-    }else if(INDIRECTOS0<=nblogico<INDIRECTOS1){ //Dentro de los bloques indirectos 1.
+    }else if((INDIRECTOS0<=nblogico)&&(nblogico<INDIRECTOS1)){ //Dentro de los bloques indirectos 1.
         if(nivel_punteros=2){
             return (nblogico-INDIRECTOS0)/NPUNTEROS;
         }else if(nivel_punteros=1){
@@ -570,7 +570,7 @@ int obtener_indice(unsigned int nblogico, int nivel_punteros){
             fprintf(stderr, RED"Error, el nivel del puntero no es correcto.  \n"RESET);
             return FALLO;
         }
-    }else if(INDIRECTOS1<=nblogico<INDIRECTOS2){ //Dentro de los bloques indirectos 2.
+    }else if((INDIRECTOS1<=nblogico)&&(nblogico<INDIRECTOS2)){ //Dentro de los bloques indirectos 2.
             if(nivel_punteros=3){
                 return (nblogico-INDIRECTOS1)/(NPUNTEROS*NPUNTEROS);
             }else if(nivel_punteros=2){
