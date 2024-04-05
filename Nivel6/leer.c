298da9bd79d5cd4a58a365cc3306689543347762
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-04-05 14:38:36 +0200

    Update leer.c

--- Nivel6/leer.c
+++ Nivel6/leer.c
@@ -2,19 +2,19 @@
 
 #define tambuffer 1500 //Tamaño del buffer de lectura
 
-int main(int argc, int **argv) {
+int main(int argc, const char *argv[]) {
 
-    if (argc=!3){
+    if (argc!=3){
         fprintf(stderr, RED "Sintaxis: permitir <nombre_dispositivo> <ninodo>\n"RESET);
         return FALLO;
     }
 
-    unsigned char dispositivo = argv[1];
+    //const char dispositivo = argv[1];
     int ninodo = atoi(argv[2]);
     
     //montar el dispositivo
 
-    if (bmount(dispositivo)==FALLO){
+    if (bmount(argv[1])==FALLO){
           fprintf(stderr, RED "Se ha producido un error al montar el dispositivo(leer.c).\n"RESET);
             return FALLO;
     }
@@ -33,20 +33,21 @@ int main(int argc, int **argv) {
     int numBytesLeidos = 0;
     int leidos = 0;
     memset(buffer_texto,0,tambuffer);
-        //obtener los bytes leedos
+        //obtener los bytes leIdos
     leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer);
     if ( leidos < 0){
         fprintf(stderr, RED"Se ha producido un error al leer el bloque\n"RESET);
         return FALLO;
     }
-    //numBytesLeidos += leidos;
     
     while (leidos > 0) {
         numBytesLeidos += leidos;
         write(1, buffer_texto, leidos);
         memset(buffer_texto,0,tambuffer);
         offset += tambuffer;
-        if (leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer) < 0){
+        leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer) ;
+
+        if (leidos < 0){
             fprintf(stderr, RED"Se ha producido un error al leer el bloque\n"RESET);
             return FALLO;
         }
@@ -55,7 +56,7 @@ int main(int argc, int **argv) {
    struct inodo inodo;
     if (leer_inodo(ninodo, &inodo) == FALLO)
     {
-        fprintf(stderr, "Error main leer_inodo");
+        fprintf(stderr, RED"Error al leer el inodo (leer.c).\n"RESET);
         return FALLO;
     }
 
@@ -64,7 +65,7 @@ int main(int argc, int **argv) {
         write(2, string, strlen(string));
   
     if (bumount() == FALLO) {
-        fprintf(stderr, "Error main bumount");
+        fprintf(stderr, RED"Error al desmontar el dispositivo.\n"RESET);
         return FALLO;
     }
 return EXITO;
