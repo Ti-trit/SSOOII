125ecc37323c112d87e26c2300df4621e6d0a11a
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-04-05 14:38:19 +0200

    Update leer.c

--- Nivel5/leer.c
+++ Nivel5/leer.c
@@ -9,12 +9,12 @@ int main(int argc, const char *argv[]) {
         return FALLO;
     }
 
-    const char dispositivo = argv[1];
+    //const char dispositivo = argv[1];
     int ninodo = atoi(argv[2]);
     
     //montar el dispositivo
 
-    if (bmount(dispositivo)==FALLO){
+    if (bmount(argv[1])==FALLO){
           fprintf(stderr, RED "Se ha producido un error al montar el dispositivo(leer.c).\n"RESET);
             return FALLO;
     }
@@ -33,13 +33,12 @@ int main(int argc, const char *argv[]) {
     int numBytesLeidos = 0;
     int leidos = 0;
     memset(buffer_texto,0,tambuffer);
-        //obtener los bytes leedos
+        //obtener los bytes leIdos
     leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer);
     if ( leidos < 0){
         fprintf(stderr, RED"Se ha producido un error al leer el bloque\n"RESET);
         return FALLO;
     }
-    //numBytesLeidos += leidos;
     
     while (leidos > 0) {
         numBytesLeidos += leidos;
@@ -57,7 +56,7 @@ int main(int argc, const char *argv[]) {
    struct inodo inodo;
     if (leer_inodo(ninodo, &inodo) == FALLO)
     {
-        fprintf(stderr, "Error main leer_inodo");
+        fprintf(stderr, RED"Error al leer el inodo (leer.c).\n"RESET);
         return FALLO;
     }
 
@@ -66,7 +65,7 @@ int main(int argc, const char *argv[]) {
         write(2, string, strlen(string));
   
     if (bumount() == FALLO) {
-        fprintf(stderr, "Error main bumount");
+        fprintf(stderr, RED"Error al desmontar el dispositivo.\n"RESET);
         return FALLO;
     }
 return EXITO;
