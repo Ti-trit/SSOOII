0ed68f3d99f9c217cc2af4a29558310c058448a4
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-04-12 23:45:21 +0200

    Update directorios.c

--- Nivel7/directorios.c
+++ Nivel7/directorios.c
@@ -1,5 +1,13 @@
 #include "directorios.h"
-
+/**
+ * @brief Función que separa una cadena en dos partes, donde la parte inicial representa
+ * un directorio.
+ * @param   camino    cadena a separar
+ * @param   inicial   parte inicial obtenido de camino
+ * @param   final     resta de la cadena camino
+ * @param   tipo      tipo del camino
+ * @return            EXITO o FALLO  
+*/
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
     if (camino[0] != '/') {
         fprintf(stderr, RED"Error, el camino no contiene / al inicio\n" RESET);
@@ -10,7 +18,11 @@ int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
     if (segundo_slash != NULL) {
         // Extraer la posicion hasta el segundo '/'
         int len = segundo_slash - (camino + 1);
-        strncpy(inicial, camino + 1, len);
+
+        if (strncpy(inicial, camino + 1, len)==NULL){
+            perror("extraer_camino:Error strncpy()");
+            return FALLO;
+        }
         inicial[len] = '\0';
 
         //Resto del camino
@@ -18,7 +30,11 @@ int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
         *tipo = 'd'; //Es un directorio porque hay un segundo '/'
     } else {
         //No hay segunco '/', por lo tanto es un fichero
-        strcpy(inicial, camino + 1);
+        if (strcpy(inicial, camino + 1)==NULL){
+            perror("extraer_camino:Error strncpy()");
+            return FALLO;
+        }
+        
         final[0] = '\0';
         *tipo = 'f';
     }
