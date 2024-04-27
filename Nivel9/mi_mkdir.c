1a15d75ca2accc0b8e6a6d757e13be985c5c78bf
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-04-27 14:27:45 +0200

    Add files via upload

---
+++ Nivel9/mi_mkdir.c
@@ -0,0 +1,45 @@
+#include "directorios.h"
+
+/**
+ * Programa que crea un fichero o directorio
+*/
+int main (int argc, char *argv[]){
+    //comporbar sintaxis
+    if (argc!=4){
+        fprintf(stderr, RED "Sintaxis: ./mi_mkdir <nombre_dispositivo> <permisos> </ruta_directorio/>\n " RESET);
+        return FALLO;
+    }
+   
+
+    //comprobar si termina en '/'
+    char * camino = argv[3];
+    if (camino[strlen(camino) - 1] != '/') {
+        fprintf(stderr, RED"La ruta no termina en '/'\n"RESET);
+        return FALLO;
+    }
+    int permisos = atoi(argv[2]);
+    if (permisos < 0 || permisos > 7) {
+        fprintf(stderr, RED "Error: modo inválido<<%i>>\n" RESET, permisos);
+        return FALLO;
+    }
+
+     //montar el dispositivo
+    if (bmount(argv[1])<0){
+        fprintf(stderr, RED "mi_mkdir.c: Error al montar el dispositivo\n"RESET);
+        return FALLO;
+    }
+
+    int error = mi_creat(argv[3], permisos); 
+    if (error< 0) {
+       mostrar_error_buscar_entrada(error);
+        return FALLO;
+    }
+     //desmontar el dispositivo
+    if (bumount()==FALLO){
+        fprintf(stderr, RED "mi_mkdir.c: Error al desmontar el dispositivo\n"RESET);
+        return FALLO;
+    }
+
+    return EXITO;
+
+}
