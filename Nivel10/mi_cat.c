6db14d9ee39aa57a25635fbb67048b1b56ff1aed
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-05-06 22:22:44 +0200

    Add files via upload

---
+++ Nivel10/mi_cat.c
@@ -0,0 +1,83 @@
+#include "directorios.h"
+#define tambuffer 1500
+/**
+ * Programa que muestra todo el contenido de uun fichero
+*/
+
+
+int main (int argc, char ** argv){
+
+
+    //comprobamos la sintaxis
+    if (argc!=3){
+        fprintf(stderr, RED "Sintaxis: mi_cat <disco> </ruta_fichero>\n"RESET);
+        return FALLO;
+    }
+
+    //montar el disco
+
+if (bmount(argv[1])==FALLO){
+          fprintf(stderr, RED "mi_cat.c: Error al montar el disco\n"RESET);
+            return FALLO;
+    }
+
+    //comprobamos que la ruta de un fichero
+    char * ruta = (char*)argv[2];
+    if (ruta[strlen(ruta)-1]=='/'){
+        fprintf(stderr, RED "mi_cat.c: La ruta no es correcta\n"RESET);
+        return FALLO;
+    }
+unsigned char buffer[tambuffer];
+memset(buffer, 0, tambuffer);
+int offset = 0;
+int leidos_aux = 0;
+int bytes_leidos =0;
+     leidos_aux = mi_read(ruta, buffer, offset, tambuffer);
+while (leidos_aux>0){//mientras hay contenido en el fichero 
+    bytes_leidos+=leidos_aux; 
+       //imprimir por pantalla el contenido del fichero
+        write(1, buffer, leidos_aux);
+
+       // printf("%s", buffer);
+      //actualizamos el offset de lectura
+
+    offset+=leidos_aux;
+    memset(buffer, 0, tambuffer);
+    leidos_aux=mi_read(ruta, buffer,offset, tambuffer);
+}
+
+if (bytes_leidos<0){
+     mostrar_error_buscar_entrada(bytes_leidos);
+        bytes_leidos = 0;
+}
+
+// struct superbloque SB;
+// if (bread(posSB, &SB)<0){
+//     fprintf(stderr, RED "Error al leer el superbloque\n"RESET);
+//     return FALLO;
+// }
+// //buscamos el inodo del fichero
+// unsigned int p_inodo_dir=0, p_inodo=0, p_entrada=0;
+// struct inodo inodo;
+// int error = buscar_entrada(ruta, p_inodo_dir,p_inodo,p_entrada,0,2);
+// if (error<0){
+//     mostrar_error_buscar_entrada(error);
+//     return FALLO;
+// }
+// if (leer_inodo(p_inodo,&inodo)==FALLO){
+//     fprintf(stderr, RED "mi_cat.c: Error al leer el inodo del fichero\n"RESET);
+//     return FALLO;
+// }
+// if (bytes_leidos!=inodo.tamEnBytesLog){
+//     fprintf(stderr, RED "El tamaño de bytes leidos no coincide con el tamaño de bytes del fichero\n"RESET);
+// }
+fprintf(stdout, "\nTotal_leidos : %i\n", bytes_leidos);
+
+    //desmontar el disco
+    if (bumount()==FALLO){
+        fprintf(stderr, RED RESET);
+        return FALLO;
+    }
+
+return EXITO;
+}
---
+++ Nivel10/mi_ls.c
@@ -0,0 +1,102 @@
+#include "directorios.h"
+#define TAMFILA 100
+#define TAMBUFFER (TAMFILA*1000) //suponemos un máx de 1000 entradas, aunque debería ser SB.totInodos
+
+/**
+ * Programa que lista el contenido de una directior/fichero
+ * Se ha añadido el parametro formato. 0 para formato simple, y 1 para formato expandido
+ * Se puede usar también para los ficheros.
+*/
+int main(int argc, char const *argv[]){
+
+    if(argc != 4){
+        fprintf(stderr, RED "Error de sintaxis. Uso correcto: ./mi_ls <disco> </ruta_directorio> <formato>\n" RESET);
+        return FALLO;
+    }
+
+    // Obtener argumentos
+    char *camino = (char*)argv[2];
+    int formato = atoi(argv[3]); // formato 0 es simple, diferente de 0 es expandido
+
+    // Montaje del dispositivo virtual
+    if (bmount(argv[1]) == FALLO) {
+        fprintf(stderr, RED "Error al montaje el dispositivo virtual\n" RESET);
+        return FALLO;
+    }
+
+
+char buff[TAMBUFFER];
+memset(buff, 0, TAMBUFFER);
+
+char tipo;
+if (camino[strlen(camino)-1] != '/') {
+    tipo = 'f';
+} else {
+    tipo = 'd';
+}
+
+if (tipo == 'f') {
+    int index = strlen(camino) - 1;
+    while (index >= 0) {
+        if (camino[index] == '/') {
+            break;
+        }
+        index--;
+    }
+
+    char *nuevoCamino = malloc(index + 1); // Asignar memoria para nuevoCamino
+    strncpy(nuevoCamino, camino, index+1);
+    nuevoCamino[index+1] = '\0'; // Marcar fin de cadena con '\0'
+
+    char *nombreFichero = malloc(strlen(camino) - index); // Asignar memoria para nombreFichero
+    strncpy(nombreFichero, camino + index + 1, strlen(camino) - index);
+    nombreFichero[strlen(camino) - index] = '\0'; 
+    int numEntradas = mi_dir(nuevoCamino, buff, 'd');
+    char *linea = strtok(buff, "\n"); // Obtener la primera línea del buffer
+    for (int i = 0; i < numEntradas; i++) {
+        if (strstr(linea, nombreFichero) != NULL) {
+            fprintf(stdout, "Tipo\tModo\tmTime\t\t\tTamaño\tNombre\n--------------------------------------------------------------------------------\n%s\n", linea);
+            break;
+        }
+        linea = strtok(NULL, "\n"); // Avanzar a la siguiente línea
+    }
+// Liberar la memoria asignada
+    free(nuevoCamino); 
+    free(nombreFichero);
+}else{
+
+
+    memset(buff, 0, TAMBUFFER);
+   
+    int numEntradas = mi_dir(argv[2], buff, tipo);
+  //  fprintf(stdout, BLUE "total entradas de %s es : %i\n"RESET, argv[2], numEntradas);
+    if (numEntradas < 0) {
+        mostrar_error_buscar_entrada(numEntradas);
+        return FALLO;
+    } else if (numEntradas > 0) {
+    if (formato == 0) {//formato simple
+    char *linea = strtok(buff, "\n"); // Obtener la primera línea del buffer
+    for (int i = 0; i < numEntradas && linea != NULL; i++) {
+        char *ultimoTab = strrchr(linea, '\t'); // Encontrar el último tabulador
+       if (ultimoTab != NULL) {
+            fprintf(stdout, "%.*s", 5, linea); // Imprimir directamente el color (los primeros 5 caracteres)
+            fprintf(stdout, "%s\t", ultimoTab + 1); // Imprimir el nombre de la entrada
+        }
+        linea = strtok(NULL, "\n"); // Avanzar a la siguiente línea
+    }
+            fprintf(stdout, RESET "\n");
+        } else {//formato expandido
+            if (tipo == 'd') {//solo imprimos el total en caso de directorios
+                fprintf(stdout, "Total: %d\n",numEntradas);
+            }
+            fprintf(stdout, "Tipo\tModo\tmTime\t\t\tTamaño\tNombre\n--------------------------------------------------------------------------------\n%s\n", buff);
+        }
+    }
+}
+    
+    if (bumount() == FALLO){
+        fprintf(stderr, RED "Error al desmontar el dispositivo virtual.\n" RESET);
+        return FALLO;
+    }
+    return EXITO;
+}
