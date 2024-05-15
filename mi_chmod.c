eea4fe355a4a0a026c55c4cda155b1947727a749
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2024-05-15 22:07:27 +0200

    Add files via upload

---
+++ mi_chmod.c
@@ -0,0 +1,45 @@
+#include "directorios.h"
+
+/**
+ * Programa que cambia los permisos de un fichero/directorio, llamando a la función mi_chmod().
+ * 
+*/
+
+int main (int argc, char ** argv){
+    if (argc!=4){
+        fprintf(stderr, RED "Sintaxis: ./mi_chmod <nombre_dispositivo> <permisos> </ruta>\n"RESET);
+        return FALLO;
+    }
+
+    //comprobamos que los permisos son correctos
+
+    //los permisos se indican en octal
+
+    unsigned char permisos = atoi(argv[2]);
+    if (permisos<0 || permisos >7){
+
+        fprintf(stderr, RED "Error: permisos no validos\n"RESET);
+        return FALLO;
+    }
+
+ // Montar  del dispositivo virtual
+    if (bmount(argv[1]) == FALLO) {
+        fprintf(stderr, RED "mi_chmod.c: Error al montar el dispositivo\n" RESET);
+        return FALLO;
+    }
+    int error = mi_chmod(argv[3], permisos);
+    if (error<0){
+        mostrar_error_buscar_entrada(error);
+        return FALLO;
+    }
+
+
+if (bumount() == FALLO){
+        fprintf(stderr, RED "mi_chmod.c: Error al desmontar el dispositivo\n" RESET);
+        return FALLO;
+    }
+
+
+
+    return EXITO;
+}
